6df5183158d6d1ca5f41a4f4a8f1a86c189750f9
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-06-20 12:00:05 +0200

    fixture faker for admin

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Faker\Factory;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(Private UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+
+        $user = new User();
+        $user->setUsername("Eldryr62570")
+        ;
+
+        //Tout les users générés auront comme mdp "test"
+        $plaintextPassword = "test";
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        $manager->persist($user);
+       
+    }
+}
